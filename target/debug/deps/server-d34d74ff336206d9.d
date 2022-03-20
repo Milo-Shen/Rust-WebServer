863da1d5734c72414b34553de39d23bde2cdceeb
MiloShen <Milo-Shen@outlook.com>
2022-03-20 14:41:43 +0800

    feat: delete useless code

--- target/debug/deps/server-d34d74ff336206d9.d
+++
@@ -1,5 +0,0 @@
-/Users/sllimen/Desktop/WorkStation/Rust/Rust-WebServer/target/debug/deps/server-d34d74ff336206d9: src/main.rs
-
-/Users/sllimen/Desktop/WorkStation/Rust/Rust-WebServer/target/debug/deps/server-d34d74ff336206d9.d: src/main.rs
-
-src/main.rs:
--- target/debug/server.d
+++
@@ -1,1 +0,0 @@
-/Users/sllimen/Desktop/WorkStation/Rust/Rust-WebServer/target/debug/server: /Users/sllimen/Desktop/WorkStation/Rust/Rust-WebServer/src/main.rs
